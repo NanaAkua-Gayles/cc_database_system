96b5929085a6e53eca2688c65d2e412f2efb333c
NanaAkua-Gayles <aaninsin@st.ug.edu.gh>
2024-12-31 02:41:24 +0000

    "adjusted some files for a responsive design"

--- member.php
+++ member.php
@@ -1,46 +1,17 @@
 <?php
-include 'db_conn.php';
-
-header('content-Type: application/json');
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-switch ($method) {
-    case 'GET':
-        if (isset($_GET['id'])) {
-            $stmt = $pdo->prepare("SELECT  * FROM members WHERE id = ?");
-            $stmt->execute([$_GET['id']]);
-            $member = $stmt->fetch(PDO::FETCH_ASSOC);
-            echo json_encode($member);
-        } else {
-            $stmt = $pdo->query("SELECT  * FROM members");
-            $members = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            echo json_encode($members);
-        }
-        break;
-
-    case 'POST':
-        $data = json_decode(file_get_contents("php://input"), true);
-        $stmt = $pdo->prepare("INSERT INTO members (title, fullName, picture, gender, dob, phone, 
-        emailStatus, address, nationality, education, profession, emergencyName, emergencyContact,
-         team, department) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-         $stmt-> execute(array_values($data));
-         echo json_encode($data);
-         break;
-
-         case 'PUT':
-            $data = json_decode(file_get_contents("php://input"), true);
-            $stmt = $pdo->prepare("UPDATE members SET title = ?, fullName = ?, picture = ?, gender = ?, dob = ?, phone = ?, 
-        emailStatus = ?, address = ?, nationality = ?, education = ?, profession = ?, emergencyName = ?, emergencyContact = ?,
-         team = ?, department = ? WHERE id = ?"); 
-         $stmt-> execute([...array_values($data), $data['id']]);
-         echo json_encode($data);
-         break;
-
-         case 'DELETE':
-            $stmt = $pdo->prepare("DELETE FROM members WHERE id = ?");
-            $stmt->execute([$_GET['id']]);
-            echo json_encode(['deleted' => true]);
-            break;
-}
+        $title = $_POST['firstName'];
+        $fullName = $_POST['firstName'];
+        $picture = $_POST['firstName'];
+        $gender = $_POST['firstName'];
+        $dob = $_POST['firstName'];
+        $phone = $_POST['firstName'];
+        $email = $_POST['firstName'];
+        $address = $_POST['firstName'];
+        $maritalStatus = $_POST['firstName'];
+        $nationality = $_POST['firstName'];
+        $education = $_POST['firstName'];
+        $profession = $_POST['firstName'];
+        $emergencyContact = $_POST['firstName'];
+        $team = $_POST['firstName'];
+        $department = $_POST['firstName'];
 ?>
